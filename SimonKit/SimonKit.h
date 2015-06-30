3a8e3aa4a0b57edcdf71efe5d381a77701307cfa
Alexsander Akers <me@a2.io>
2015-06-30 15:15:25 +0100

    Add SimonKit iOS/watchOS framework target

---
+++ SimonKit/SimonKit.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimonKit.
+FOUNDATION_EXPORT double SimonKitVersionNumber;
+
+//! Project version string for SimonKit.
+FOUNDATION_EXPORT const unsigned char SimonKitVersionString[];
